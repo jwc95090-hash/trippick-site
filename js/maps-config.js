8951c6b45c9bb4c768beb8e21b2ff73ff6e8fa83
jwc95090-hash <jwc95090@gmail.com>
2026-08-26 17:25:22 +0900

    Correct Google Maps key casing

--- js/maps-config.js
+++ js/maps-config.js
@@ -2,4 +2,4 @@
 // This browser key is public by design. Restrict it in Google Cloud to:
 //   https://jwc95090-hash.github.io/trippick-site/*
 // and allow only the Maps JavaScript API.
-window.TRIPPICK_GOOGLE_MAPS_KEY = 'AIzaSyCh2ZiBsBPVm2utpT2Df8ckJrqqauQt6NI';
+window.TRIPPICK_GOOGLE_MAPS_KEY = 'AIzaSyCh2ZiBsBPVm2utpT2Df8ckJrqqaUQt6NI';
